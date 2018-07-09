be500ed98122826b9923aab37362aa8eeffc2a50
Kari Mcfarlin <40720949+morri196@users.noreply.github.com>
2018-07-08 20:53:31 -0400

    Update Numbers.java

--- src/NumberCumputing/Numbers.java
+++ src/NumberCumputing/Numbers.java
@@ -1,48 +1,60 @@
 import java.util.Scanner;
-
+//NumberCompatibility *Spell Check*
 public class Numbers {
 	public static void main(String [] args){
-		Scanner scnr = new Scanner(System.in);
-		int hundredsPlace;
-		int tensPlace;
-		int onesPlace;
-		int hundredsPlace2;
-		int tensPlace2;
-		int onesPlace2;
-		int totalA;
-		int totalB;
-		int totalC;
-		boolean numCompatibility; //boolean because it is either true or false depending on computation
+	Scanner scnr = new Scanner(System.in);
 		
-		hundredsPlace = scnr.nextInt();
-		tensPlace = scnr.nextInt();
-		onesPlace = scnr.nextInt();
-		hundredsPlace2 = scnr.nextInt();
-		tensPlace2 = scnr.nextInt();
-		onesPlace2 = scnr.nextInt();
-		// we will take input from user, which is random three digit number 
+		int userInput1;
+		int userInput2;
+		int stCount;
+		int i;
+		String userInputtxt1;
+		String userInputtxt2;
+		char[] userInputtxt1AR;
+		char[] userInputtxt2AR;
+		boolean ansEqual = true;
+	
+		System.out.println("Enter First Three Digit Number:   ");
+		userInput1 = scnr.nextInt();
 		
-		System.out.println("Enter First Number:   " + (hundredsPlace + tensPlace + onesPlace));
+		System.out.println("Enter Second Three Digit Number:   ");
+		userInput2 = scnr.nextInt();
 		
-		System.out.println("Enter Second Number:   " + (hundredsPlace2 + tensPlace2 + onesPlace2));
+		userInputtxt1 = Integer.toString(userInput1);//converts interger to string
+		userInputtxt2 = Integer.toString(userInput2);
 		
-		//we then want to total numbers here to see if the totals for each number place matches
-		totalA = hundredsPlace + hundredsPlace2;
-		totalB = tensPlace + tensPlace2;
-		totalC = onesPlace + onesplace2;
+		stCount = userInputtxt1.length(); //set string count
+		userInputtxt1AR = userInputtxt1.toCharArray();//those 20 characters now goes into my array
 		
+		stCount = userInputtxt2.length(); 
+		userInputtxt2AR = userInputtxt2.toCharArray();
 		
-		/*I thought of doing a for loop but i didnt find it fitting (for (i = 0;i < 3; i++)) 
-		 * instead I did and if/ else and initialized "numcompatibility" on a conditional basis*/
+		if (userInputtxt1.length() != userInputtxt2.length()) { //this prevents user invalid entry
+			System.out.println("Invalid Entry");
+		}
+		else {
+			
 		
-		if (totalA == totalB, totalB == totalC, totalA == totalC) {
-			numCompatibility = True;}
-			else {
-				numCompatibility = false;
+
+		for (i = 0; i < stCount-1; i = i + 1) { // stCount-1 because we dont want the loop to exceed array size
+			userInput1 = Character.getNumericValue(userInputtxt1AR[i]);//this statement converts char to int
+			userInput2 = Character.getNumericValue(userInputtxt2AR[i]);
+			
+			if (Character.getNumericValue(userInputtxt1AR[i]) + Character.getNumericValue(userInputtxt2AR[i]) != 
+					Character.getNumericValue(userInputtxt1AR[i + 1]) + Character.getNumericValue(userInputtxt2AR[i + 1])) {
+		ansEqual = false; //the if statement is saying if each corresponding placevalue is not equal to ach other then false
 			}
-		System.out.print(numCompatibility);
-				
+		}
+			if (ansEqual) { // has ansEqual ever had the value "false", if not print "true"
+			System.out.println("True"); 
+			}
+			else {
+				System.out.println("false"); //or print false
+			}
+			
+			
 		
+		}		
 	}
 
 }
